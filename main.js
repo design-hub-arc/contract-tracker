56266e206be7e0f635f02902a2e62c87fac1fe54
Devin Scholl <DevinScholl1@gmail.com>
2018-10-22 20:48:19 -0700

    Added express routes for static and runtime generated content

--- main.js
+++ main.js
@@ -1,7 +1,81 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const cons = require('consolidate');
+const bodyParser = require('body-parser');
+const url = require('url');
+
 const app = express();
 const port = 8080;
 
-app.get('/*', (req, res) => res.send('Testing Express!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// helper function
+var static_middleware = (path_to_static_files) => {
+    return express.static(path.join(__dirname, path_to_static_files));
+};
+
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// assign the ejs engine to .html files
+app.engine('ejs', cons.ejs);
+
+// set .ejs as the default extension
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/build/server/ejs');
+
+
+// logging
+app.use((req, res, next) => {
+    console.log("%s | %s | %s | %s | %s",
+                req.headers.host,
+                new Date().toString(),
+                req.method,
+                req.url,
+                req.headers['user-agent']);
+    next();
+});
+
+
+// css
+app.get('/css/*', static_middleware('build/css'));
+
+// html
+app.get('/html/*', static_middleware('build/html'));
+
+// js
+app.get('/js/*', static_middleware('build/js'));
+
+// static
+app.get('/static/*', static_middleware('build/static'));
+
+
+// server
+//// ejs
+app.get('/server/ejs/*', (req, res, next) => {
+    res.render(req.params[0], {site_name: 'Design Hub Contract Tracker'});
+});
+
+
+//// Error handler
+app.use((err, req, res, next) => {
+    res.sendStatus(404);
+});
+
+
+//// js
+app.post('/server/js/', (req, res, next) => {
+});
+
+
+// No route found
+app.use((req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
