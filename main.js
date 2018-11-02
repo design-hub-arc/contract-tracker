c3d03aca6eecbabff5c925f8fb03f0f2cec524cb
Devin Scholl <DevinScholl1@gmail.com>
2018-11-02 01:01:10 -0700

    Added Cookie Parser

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const path = require('path');
 const ejs = require('ejs');
 const cons = require('consolidate');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const url = require('url');
 
 const app = express();
@@ -14,6 +15,9 @@ var static_middleware = (path_to_static_files) => {
     return express.static(path.join(__dirname, path_to_static_files));
 };
 
+
+// for parsing cookies
+app.use(cookieParser());
 
 // for parsing application/json
 app.use(bodyParser.json());
@@ -55,20 +59,23 @@ app.use('/js', static_middleware('build/js'));
 app.use('/static', static_middleware('build/static'));
 
 
-// server
-//// ejs
+// Error handler
+app.use((err, req, res, next) => {
+    res.sendStatus(500);
+});
 app.get('/server/ejs/*', (req, res, next) => {
+    // sends file to user
     res.render(req.params[0], {site_name: 'Design Hub Contract Tracker'});
 });
 
 
-//// Error handler
+// Error handler
 app.use((err, req, res, next) => {
     res.sendStatus(404);
 });
 
 
-//// js
+// js
 app.post('/server/js/', (req, res, next) => {
 });
 
