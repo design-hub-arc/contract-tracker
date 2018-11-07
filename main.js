70b2e3c7d4257ddbc53f22a3f7e8f7098726d596
Devin Scholl <DevinScholl1@gmail.com>
2018-11-07 14:04:08 -0800

    Minor changes

--- main.js
+++ main.js
@@ -9,30 +9,15 @@ const url = require('url');
 const app = express();
 const port = process.env.PORT || 8080;
 
-
-
-// helper function
-var static_middleware = (path_to_static_files) => {
-    return express.static(path.join(__dirname, path_to_static_files));
-};
-
-
-// for parsing cookies
-app.use(cookieParser());
-
-// for parsing application/json
-app.use(bodyParser.json());
-
-// for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// changes working directory of the process to the directory this file is in
+process.chdir(__dirname);
 
 // assign the ejs engine to .html files
 app.engine('ejs', cons.ejs);
 
 // set .ejs as the default extension
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/build/server/ejs');
+app.set('views', '/build/server/ejs');
 
 
 // logging
@@ -47,28 +32,39 @@ app.use((req, res, next) => {
 });
 
 
-// Error handler
-app.use((err, req, res, next) => {
-    res.sendStatus(500);
 });
+
 
 // css
-app.use('/css', static_middleware('build/css'));
+app.use('/css',    express.static('build/css'));
 
 // html
-app.use('/html', static_middleware('build/html'));
+app.use('/html',   express.static('build/html'));
 
 // js
-app.use('/js', static_middleware('build/js'));
+app.use('/js',     express.static('build/js'));
 
 // static
-app.use('/static', static_middleware('build/static'));
+app.use('/static', express.static('build/static'));
 
 
 // Error handler
 app.use((err, req, res, next) => {
-    res.sendStatus(500);
+    res.sendStatus(404);
 });
+
+
+
+// for parsing cookies
+app.use(cookieParser());
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // ejs
 app.get('/server/ejs/*', (req, res, next) => {
     // sends file to user
@@ -79,11 +75,6 @@ app.get('/server/ejs/*', (req, res, next) => {
 // Error handler
 app.use((err, req, res, next) => {
     res.sendStatus(500);
-});
-
-
-// js
-app.post('/server/js/', (req, res, next) => {
 });
 
 
