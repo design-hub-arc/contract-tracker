a5a42a2262d2c21cb9a2f9eebb454c16244f6efb
Devin Scholl <DevinScholl1@gmail.com>
2018-11-15 22:48:46 -0800

    Added CookieParser npm package and included session handling

--- src/server/js/view/auth.js
+++ src/server/js/view/auth.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const fb_auth = require('../model/services/firebaseauthapi.js');
+const session = require('../controller/sessions.js');
 
 const router = express.Router();
 
 
 // for parsing application/json
 router.use(bodyParser.json());
+
+// for parsing cookies
+router.use(cookieParser());
+
 
 // log in user
 router.post('/login', (req, res, next) => {
