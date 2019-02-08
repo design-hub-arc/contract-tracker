eddafcdebe03b529c7c0f13b2bbbf24b28035e59
Devin Scholl <DevinScholl1@gmail.com>
2019-02-08 11:55:49 -0800

    Server Markup task was compiling the wrong directories

--- tasks/server/markup.js
+++ tasks/server/markup.js
@@ -5,15 +5,15 @@ const { dest }      = require('gulp');
 // Copy server .ejs files to the build directory
 function copyServerEJS()
 {
-  return src("src/server/html/complete/**/*.ejs")
-    .pipe(dest("build/server/html/"));
+  return src("src/server/ejs/complete/**/*.ejs")
+    .pipe(dest("build/server/ejs/"));
 }
 
 
 // Copy server .html files to the build directory
 function copyServerHTML()
 {
-  return src("src/server/html/complete/**/*.html")
+  return src("src/server/ejs/complete/**/*.html")
     .pipe(dest("build/server/html/"));
 }
 
