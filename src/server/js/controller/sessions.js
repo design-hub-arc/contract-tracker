a05fbfc5037f3124d6c0786db8a109c3c3715065
Devin Scholl <devinscholl1@gmail.com>
2019-02-12 20:27:29 -0800

    Changed indent from 4 spaces to 2

--- src/server/js/controller/sessions.js
+++ src/server/js/controller/sessions.js
@@ -14,8 +14,8 @@ var sessions = {};
 
 async function verify (token)
 {
-    if (!public_key) throw "NO_PUBLIC_KEY_PROVIDED";
-    return jwt.verify(token, public_key, {algorithms: ['RS256'], ignoreExpiration: true});
+  if (!public_key) throw "NO_PUBLIC_KEY_PROVIDED";
+  return jwt.verify(token, public_key, {algorithms: ['RS256'], ignoreExpiration: true});
 }
 
 module.exports.verify = verify;
@@ -23,20 +23,20 @@ module.exports.verify = verify;
 
 async function create (uid)
 {
-    // get unique token id (Not guranteed but nearly impossible to occur)
-    // the chance of a collision is: 1 in roughly 3.4E^38
-    var token_uid = uuid();
+  // get unique token id (Not guranteed but nearly impossible to occur)
+  // the chance of a collision is: 1 in roughly 3.4E^38
+  var token_uid = uuid();
 
-    // set expiration date to +30m
-    var payload = {
-        sub: uid,
-        iat: Date.now(),
-        exp: new Date(Date.now() + 1000 * 60 * 30).getTime(),
-        jti: token_uid
-    };
+  // set expiration date to +30m
+  var payload = {
+    sub: uid,
+    iat: Date.now(),
+    exp: new Date(Date.now() + 1000 * 60 * 30).getTime(),
+    jti: token_uid
+  };
 
-    if (!private_key) throw "NO_PRIVATE_KEY_PROVIDED";
-    return {encoded: jwt.sign(payload, private_key, jwt_options), payload: payload};
+  if (!private_key) throw "NO_PRIVATE_KEY_PROVIDED";
+  return {encoded: jwt.sign(payload, private_key, jwt_options), payload: payload};
 }
 
 module.exports.create = create;
@@ -44,12 +44,12 @@ module.exports.create = create;
 
 async function revoke (token, verify=true)
 {
-    if (verify)
-    {
-        // verify token is valid
-    }
+  if (verify)
+  {
+    // verify token is valid
+  }
 
-    // delete token from sessions
+  // delete token from sessions
 }
 
 module.exports.revoke = revoke;
@@ -57,11 +57,11 @@ module.exports.revoke = revoke;
 
 async function read (token, verify=true)
 {
-    if (verify)
-    {
-        // verify token is valid
-    }
+  if (verify)
+  {
+    // verify token is valid
+  }
 
-    // parse and return payload
+  // parse and return payload
 }
 module.exports.read = read;
