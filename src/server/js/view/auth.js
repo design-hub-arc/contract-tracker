3c0228fa3767ba503547e3a898bec73dee8f1791
Devin Scholl <devinscholl1@gmail.com>
2019-02-14 20:25:18 -0800

    Changed indent from 4 to 2 spaces

--- src/server/js/view/auth.js
+++ src/server/js/view/auth.js
@@ -16,91 +16,91 @@ router.use(cookieParser());
 
 // log in user
 router.post('/login', (req, res, next) => {
-    if ('session' in req.cookies)
-    {
-        let token = req.cookies['session'];
-        session.revoke(token, false)
-            .catch(err => {});
-    }
+  if ('session' in req.cookies)
+  {
+    let token = req.cookies['session'];
+    session.revoke(token, false)
+      .catch(err => {});
+  }
 
-    if ('email' in req.body && 'password' in req.body)
-    {
-        var email = req.body.email;
-        var password = req.body.password;
+  if ('email' in req.body && 'password' in req.body)
+  {
+    var email = req.body.email;
+    var password = req.body.password;
 
 
-        fb_auth.signInWithEmailAndPassword(email, password)
-            .then(user => {
-                return session.create(user.uid);
-            })
-            .then(token => {
-                res.cookie('session', token.encoded);
-                res.redirect(303, '/');
-            })
-            .catch(err => {
-                next(err);
-            });
-    }
-    else
-    {
-        next("Email and/or Password were not provided");
-    }
+    fb_auth.signInWithEmailAndPassword(email, password)
+      .then(user => {
+        return session.create(user.uid);
+      })
+      .then(token => {
+        res.cookie('session', token.encoded);
+        res.redirect(303, '/');
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+  else
+  {
+    next("Email and/or Password were not provided");
+  }
 });
 
 // sign up user
 router.post('/signup', (req, res, next) => {
 
-    if ('session' in req.cookies)
-    {
-        let token = req.cookies['session'];
-        session.revoke(token, false)
-            .catch(err => {});
-    }
+  if ('session' in req.cookies)
+  {
+    let token = req.cookies['session'];
+    session.revoke(token, false)
+      .catch(err => {});
+  }
 
-    if ('email' in req.body && 'password' in req.body)
-    {
-        var email = req.body.email;
-        var password = req.body.password;
+  if ('email' in req.body && 'password' in req.body)
+  {
+    var email = req.body.email;
+    var password = req.body.password;
 
 
-        fb_auth.signUpWithEmailAndPassword(email, password)
-            .then(user => {
-                return session.create(user.uid);
-            })
-            .then(token => {
-                res.cookie('session', token.encoded);
-                res.redirect(303, '/');
-            })
-            .catch(err => {
-                next(err);
-            });
-    }
-    else
-    {
-        next("Email and/or Password were not provided");
-    }
+    fb_auth.signUpWithEmailAndPassword(email, password)
+      .then(user => {
+        return session.create(user.uid);
+      })
+      .then(token => {
+        res.cookie('session', token.encoded);
+        res.redirect(303, '/');
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+  else
+  {
+    next("Email and/or Password were not provided");
+  }
 });
 
 
 // log out user
 router.post('/logout', (req, res, next) => {
-    if ('session' in req.cookies)
-    {
-        var token = req.cookies['session'];
-        session.read(token)
-            .then((payload) => {
-                res.clearCookie('session');
-                res.redirect(303, '/');
-                session.revoke(token);
-            })
-            .catch((err) => {
-                next(err);
-            });
-    }
-    else
-    {
-        res.sendStatus(200);
-    }
+  if ('session' in req.cookies)
+  {
+    var token = req.cookies['session'];
+    session.verify(token)
+      .then((payload) => {
+        res.clearCookie('session');
+        res.redirect(303, '/');
+        session.revoke(token);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+  else
+  {
+    res.sendStatus(200);
+  }
 });
 
 
